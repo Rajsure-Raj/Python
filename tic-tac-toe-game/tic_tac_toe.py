f4d40440fbcc6879de36dfbabd6f5f007000a742
Raj Rajsure <140630239+Rajsure-Raj@users.noreply.github.com>
2023-12-26 13:57:59 +0530

    Update tic_tac_toe.py

--- tic-tac-toe-game/tic_tac_toe.py
+++ tic-tac-toe-game/tic_tac_toe.py
@@ -1,1 +1,49 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
 
+def check_winner(board, player):
+    # Check rows, columns, and diagonals for a win
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+
+def is_board_full(board):
+    return all(board[i][j] != ' ' for i in range(3) for j in range(3))
+
+
+def tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+
+    while True:
+        print_board(board)
+        try:
+            row, col = map(int, input(f"Player {current_player}, enter row and column (0, 1, or 2) separated by space: ").split())
+        except ValueError:
+            print("Invalid input. Please enter two integers separated by a space.")
+            continue
+
+        if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
+            board[row][col] = current_player
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins! 🎉")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a tie! 😕")
+                break
+            else:
+                current_player = 'O' if current_player == 'X' else 'X'
+        else:
+            print("Invalid move. Try again.")
+
+if __name__ == "__main__":
+    print("Welcome to Tic Tac Toe!")
+    tic_tac_toe()
